d7e588fd361e242d3863b9a8a6c6f12ecea4cce5
Paul Chang <paulchang959@gmail.com>
2020-11-21 14:31:10 -0800

    add data persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -285,6 +285,24 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+// Gets task items from localStorage
+// Converts tasks from the string format back into an array of objects.
+// Iterates through a tasks array and creates task elements on the page from it.
+var loadTasks = function() {
+    var savedTasks = localStorage.getItem("tasks");
+    
+    if (!savedTasks) {
+        return false;
+    }
+    
+    savedTasks = JSON.parse(savedTasks);
+
+    // loop through savedTasks array
+    for (var i = 0; i < savedTasks.length; i++) {
+        // pass each task object into the 'createTaskEl()' function
+        createTaskEl(savedTasks[i]);
+    }
+}
 // event listeners
 
 pageContentEl.addEventListener("dragleave", dragLeaveHandler);
@@ -295,3 +313,4 @@ pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
 formEl.addEventListener("submit", taskFormHandler);
 
+loadTasks();
